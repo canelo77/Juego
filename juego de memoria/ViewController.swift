d15bff8c723f4df68f265f16a6820717ea4ad31c
Yolaidy Crisostomo <yolaidycrisostomo@macintosh-2.local>
2016-09-14 22:28:14 -0400

    Initial Commit

---
+++ juego de memoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  juego de memoria
+//
+//  Created by Yolaidy Crisostomo on 14/9/16.
+//  Copyright © 2016 Yolaidy Crisostomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
